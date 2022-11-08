3c3d7388a8f3ba4c0fe81ba9e4fefee4051b6ee7
Podter <48936015+Podter@users.noreply.github.com>
2022-11-08 18:37:21 +0700

    Update Home.tsx

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,3 +1,5 @@
+import { Github, Youtube, Facebook } from "lucide-react";
+
 export default function Home() {
   return (
     <div
@@ -8,13 +10,23 @@ export default function Home() {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">
+            Hello! I am <span className="text-ctp-red">Podter</span>
+          </h1>
           <p className="mb-5">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary btn-circle m-3">
+            <Github className="h-6 w-6" size={24} />
+          </button>
+          <button className="btn btn-primary btn-circle m-3">
+            <Facebook className="h-6 w-6" size={24} />
+          </button>
+          <button className="btn btn-primary btn-circle m-3">
+            <Youtube className="h-6 w-6" size={24} />
+          </button>
         </div>
       </div>
     </div>
