824ba2d0f643a8905d471c4e46951892213d2cd2
Podter <48936015+Podter@users.noreply.github.com>
2022-11-08 22:13:35 +0700

    moved to home dir

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,13 +1,11 @@
 import { Github, Youtube, Facebook } from "lucide-react";
 
+import Tiles from "./Tiles";
+
 export default function Home() {
   return (
-    <div
-      className="hero min-h-screen"
-      style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}
-      id="home"
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
+    <div className="hero min-h-screen" id="home">
+      <Tiles />
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">
