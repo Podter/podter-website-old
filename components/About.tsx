65695d041a7bb768c6edb3b0d46055d4be631ff3
Podter <48936015+Podter@users.noreply.github.com>
2022-11-08 18:37:28 +0700

    about

---
+++ components/About.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Arch from "../public/assets/arch.png";
+
+export default function About() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <Image
+          src={Arch}
+          className="max-w-sm rounded-lg shadow-2xl"
+          alt="arch"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">Who am I?</h1>
+          <p className="py-6">
+            Hello, I'm Podter
+            <br />
+            <br />
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores
+            illo deserunt officia nostrum quaerat quo, placeat alias maiores
+            voluptates pariatur quia excepturi aliquid sapiente, reiciendis
+            molestias repudiandae debitis ut eos!
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            placeat quo officia dicta sit deleniti ea, dolor harum expedita
+            nobis quae vitae cum accusamus. Iure odio placeat consequuntur qui
+            alias!
+          </p>
+          <button className="btn btn-primary">Check out my projects</button>
+        </div>
+      </div>
+    </div>
+  );
+}
