589933844b589957e5bbc1c48ddc2fe7d81ed7cc
Podter <48936015+Podter@users.noreply.github.com>
2022-11-08 16:08:16 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-xl text-ctp-red">Podter</a>
       </div>
       <div className="flex-none">
         <select
