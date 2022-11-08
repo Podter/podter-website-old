9dbcc1421ad6d6f03a8da9453abfa9e4aaee980f
Podter <48936015+Podter@users.noreply.github.com>
2022-11-08 17:18:34 +0700

    head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Podter</title>
+        <meta name="description" content="Podter's Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <h1>Hello world!</h1>
     </div>
   );
