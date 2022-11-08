4bf0f14c0bf7bae9f64813a5fc568b7e8a0f510e
Podter <48936015+Podter@users.noreply.github.com>
2022-11-08 15:36:36 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from "next/app";
 
+import Navbar from "../components/Navbar";
+
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
