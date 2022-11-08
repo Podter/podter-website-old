bea1a151a26d11f55bb0cd9384316288d177570a
Podter <48936015+Podter@users.noreply.github.com>
2022-11-08 16:08:23 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const { variants } = require("@catppuccin/palette");
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -12,31 +15,37 @@ module.exports = {
     themes: [
       {
         "ctp-latte": {
-          primary: "#d20f39",
-          secondary: "#1e66f5",
-          accent: "#179299",
-          neutral: "#dce0e8",
-          "base-100": "#eff1f5",
-          info: "#04a5e5",
-          success: "#40a02b",
-          warning: "#df8e1d",
-          error: "#e64553",
+          primary: variants.latte.red.hex,
+          secondary: variants.latte.blue.hex,
+          accent: variants.latte.teal.hex,
+          neutral: variants.latte.crust.hex,
+          "base-100": variants.latte.base.hex,
+          info: variants.latte.sky.hex,
+          success: variants.latte.green.hex,
+          warning: variants.latte.yellow.hex,
+          error: variants.latte.maroon.hex,
         },
       },
       {
         "ctp-mocha": {
-          primary: "#f38ba8",
-          secondary: "#89b4fa",
-          accent: "#94e2d5",
-          neutral: "#11111b",
-          "base-100": "#1e1e2e",
-          info: "#89dceb",
-          success: "#a6e3a1",
-          warning: "#f9e2af",
-          error: "#eba0ac",
+          primary: variants.mocha.red.hex,
+          secondary: variants.mocha.blue.hex,
+          accent: variants.mocha.teal.hex,
+          neutral: variants.mocha.crust.hex,
+          "base-100": variants.mocha.base.hex,
+          info: variants.mocha.sky.hex,
+          success: variants.mocha.green.hex,
+          warning: variants.mocha.yellow.hex,
+          error: variants.mocha.maroon.hex,
         },
       },
     ],
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    require("@catppuccin/tailwindcss")({
+      prefix: "ctp",
+      defaultFlavour: "latte",
+    }),
+  ],
 };
