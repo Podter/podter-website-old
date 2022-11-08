afb7c4870e9826a5cfd1905c94489d07949dd45a
Podter <48936015+Podter@users.noreply.github.com>
2022-11-08 17:18:27 +0700

    ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider
+      defaultTheme="system"
+      themes={["ctp-latte", "ctp-mocha"]}
+      value={{ light: "ctp-latte", dark: "ctp-mocha" }}
+    >
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
