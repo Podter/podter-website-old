1e5cfe86d09e67991c2d98269465fef9a6b68147
Podter <48936015+Podter@users.noreply.github.com>
2022-11-10 22:55:20 +0700

    Update CarouselItem.tsx

--- components/Projects/CarouselItem.tsx
+++ components/Projects/CarouselItem.tsx
@@ -1,14 +1,30 @@
-import Image, { ImageProps } from "next/image";
+import { Icon } from "@iconify/react";
 
-type Props = {
-  src: ImageProps["src"];
-  item: number;
-};
-
-export default function Carousel({ src, item }: Props) {
+export default function Carousel() {
   return (
-    <div id={`slide${item}`} className="carousel-item relative w-full">
-      <Image src={src} className="w-full" alt={`Slide ${item}`} />
+    <div
+      className="hero min-h-screen carousel-item"
+      id="slide3"
+      style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}
+    >
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md bg-base-100/10 backdrop-blur-xl shadow-md rounded-2xl p-8">
+          <h1 className="mb-5 text-5xl font-bold text-ctp-red">
+            Projects{" "}
+            <Icon
+              className="inline"
+              icon="fluent-emoji-flat:laptop"
+              inline={true}
+            />
+          </h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 }
