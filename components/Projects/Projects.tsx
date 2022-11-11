32c00dd99d0f1dc60287cfddffccee77174c429f
Podter <48936015+Podter@users.noreply.github.com>
2022-11-11 16:10:46 +0700

    Update Projects.tsx

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,11 +1,16 @@
+import { createContext } from "react";
 import CarouselItem from "./CarouselItem";
+
+export const SlidesContext = createContext(0);
 
 export default function Projects() {
   return (
-    <div className="carousel w-full">
-      <CarouselItem item={1} img="/assets/arch2.png" />
-      <CarouselItem item={2} img="/assets/arch.png" />
-      <CarouselItem item={3} img="/assets/arch2.png" />
-    </div>
+    <SlidesContext.Provider value={3}>
+      <div className="carousel w-full">
+        <CarouselItem item={1} img="/assets/arch2.png" />
+        <CarouselItem item={2} img="/assets/arch.png" />
+        <CarouselItem item={3} img="/assets/arch2.png" />
+      </div>
+    </SlidesContext.Provider>
   );
 }
