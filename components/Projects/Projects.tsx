cc36e38731e0c6e211e67e42b99b7b55d1f2697b
Podter <48936015+Podter@users.noreply.github.com>
2022-11-11 14:45:12 +0700

    Update Projects.tsx

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -3,9 +3,9 @@ import CarouselItem from "./CarouselItem";
 export default function Projects() {
   return (
     <div className="carousel w-full">
-      <CarouselItem item={1} />
-      <CarouselItem item={2} />
-      <CarouselItem item={3} />
+      <CarouselItem item={1} img="/assets/arch2.png" />
+      <CarouselItem item={2} img="/assets/arch.png" />
+      <CarouselItem item={3} img="/assets/arch2.png" />
     </div>
   );
 }
