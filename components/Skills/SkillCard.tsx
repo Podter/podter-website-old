dbb18d794298388b1495b60b749358f1589b2cfa
Podter <48936015+Podter@users.noreply.github.com>
2022-11-14 14:25:42 +0700

    Update SkillCard.tsx

--- components/Skills/SkillCard.tsx
+++ components/Skills/SkillCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function SkillCard({ img, name }: Props) {
   return (
-    <div className="card w-72 bg-base-100 shadow-xl">
+    <div className="card w-72 bg-base-100 shadow-xl hover:scale-105 duration-75">
       <div className="card-body text-center justify-center items-center">
         <div className="grid grid-cols-2 gap-4">
           <Image src={img} alt="Arch" width={64} height={64} />
