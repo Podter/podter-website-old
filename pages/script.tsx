12fd5cf9cb6f26c5aa2b1386102e8e0df16a6d05
Podter <48936015+Podter@users.noreply.github.com>
2022-11-14 23:00:05 +0700

    Create script.tsx

---
+++ pages/script.tsx
@@ -0,0 +1,7 @@
+import Script from "next/script";
+
+export default function ScriptPage() {
+  return (
+    <Script src="https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FPodter%2Fbrowser-screen-recorder%2Fblob%2Fmain%2FREADME.md&style=default&type=markdown&showBorder=on&showLineNumbers=on&showFileMeta=on&showFullPath=on&showCopy=on"></Script>
+  );
+}
