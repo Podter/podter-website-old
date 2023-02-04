dad0428849f9ed8b7c160ba4cce5f6d1d834f0d8
Podter <48936015+Podter@users.noreply.github.com>
2023-02-04 23:38:08 +0700

    links

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,24 +1,28 @@
 import ThemeSwitch from "./ThemeSwitch";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="navbar bg-base-100 z-50 mx-auto max-w-4xl px-8 my-20">
       <div className="flex-1">
-        <button className="btn btn-ghost normal-case text-xl bg-gradient-to-r hover:text-transparent from-ctp-red to-ctp-blue bg-clip-text transition-colors duration-300">
+        <Link
+          href="/"
+          className="btn btn-ghost normal-case text-xl bg-gradient-to-r hover:text-transparent from-ctp-red to-ctp-blue bg-clip-text transition-colors duration-300"
+        >
           Podter
-        </button>
+        </Link>
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>About</a>
+            <Link href="/about">About</Link>
           </li>
           <li>
-            <a>Projects</a>
+            <Link href="/projects">Projects</Link>
           </li>
           <li>
-            <a>Blog</a>
+            <Link href="/blog">Blog</Link>
           </li>
           <li>
-            <a>Guestbook</a>
+            <Link href="/guestbook">Guestbook</Link>
           </li>
         </ul>
       </div>
