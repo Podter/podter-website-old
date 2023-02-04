511609d3f5bab4c98c2a7c94a31245cd1914e0f9
Podter <48936015+Podter@users.noreply.github.com>
2023-02-04 21:00:54 +0700

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,6 @@
-import type { AppProps } from "next/app";
-import { ThemeProvider } from "next-themes";
-import Head from "next/head";
-import { useRouter } from "next/router";
-
-import Navbar from "../components/Navbar/Navbar";
-import Footer from "../components/Footer";
-import ScrollToTop from "../components/ScrollToTop";
-
-import "../styles/globals.css";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  if (router.route == "/theme" || router.route == "/script") {
-    return <Component {...pageProps} />;
-  }
-
-  return (
-    <ThemeProvider
-      defaultTheme="system"
-      themes={["ctp-latte", "ctp-mocha"]}
-      value={{ light: "ctp-latte", dark: "ctp-mocha" }}
-    >
-      <Head>
-        <title>Podter</title>
-        <meta name="description" content="Podter's Website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Navbar />
-      <ScrollToTop />
-      <Component {...pageProps} />
-      <Footer />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />
 }
