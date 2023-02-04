b5c4aae98ad6ed2b86e8f8ee231171fc829b4010
Podter <48936015+Podter@users.noreply.github.com>
2023-02-04 21:29:59 +0700

    added tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "@next/font/google";
 
-const poppins = Poppins({ weight: "400", subsets: ["latin"] });
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
