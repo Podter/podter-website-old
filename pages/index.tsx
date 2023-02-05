aa116d710fab0a0a2a9a62ea697a6837aec55991
Podter <48936015+Podter@users.noreply.github.com>
2023-02-05 10:50:26 +0700

    social links

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,11 @@ import Head from "next/head";
 import Container from "@/components/Container";
 import { Icon } from "@iconify/react";
 import wavingHand from "@iconify/icons-fluent-emoji-flat/waving-hand";
+import Link from "next/link";
+import githubIcon from "@iconify/icons-fa6-brands/github";
+import discordIcon from "@iconify/icons-fa6-brands/discord";
+import twitterIcon from "@iconify/icons-fa6-brands/twitter";
+import mailRounded from "@iconify/icons-material-symbols/mail-rounded";
 
 export default function Home() {
   return (
@@ -21,6 +26,32 @@ export default function Home() {
         <p className="py-6">
           Hey, I&apos;m Podter. I&apos;m a student and developer from Thailand
         </p>
+        <div className="flex flex-row gap-2 w-28">
+          <Link
+            className="btn btn-ghost btn-circle"
+            href="https://github.com/Podter"
+          >
+            <Icon icon={githubIcon} className="h-6 w-6" scale={24} />
+          </Link>
+          <Link
+            className="btn btn-ghost btn-circle"
+            href="https://discord.com/users/331793642689789962"
+          >
+            <Icon icon={discordIcon} className="h-6 w-6" scale={24} />
+          </Link>
+          <Link
+            className="btn btn-ghost btn-circle"
+            href="https://twitter.com/Real_Podter"
+          >
+            <Icon icon={twitterIcon} className="h-6 w-6" scale={24} />
+          </Link>
+          <Link
+            className="btn btn-ghost btn-circle"
+            href="mailto:me@podter.xyz"
+          >
+            <Icon icon={mailRounded} className="h-6 w-6" scale={24} />
+          </Link>
+        </div>
       </Container>
     </>
   );
