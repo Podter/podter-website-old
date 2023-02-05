0a41ca06b157ca38664f3d837c328f0da34e97df
Podter <48936015+Podter@users.noreply.github.com>
2023-02-05 10:22:03 +0700

    confetti

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,8 +2,35 @@ import ThemeSwitch from "./ThemeSwitch";
 import Link from "next/link";
 import menus from "@/lib/menus";
 import { Menu } from "lucide-react";
+import dynamic from "next/dynamic";
+import { useRef, useState, MouseEvent } from "react";
+import { useWindowSize } from "react-use";
+import { useRouter } from "next/router";
+
+const Confetti = dynamic(() => import("react-confetti"), {
+  ssr: false,
+});
 
 export default function Navbar() {
+  const confettiRef = useRef(null);
+
+  const { width, height } = useWindowSize();
+  const router = useRouter();
+
+  const [confetti, setConfetti] = useState(false);
+  const [mousePosX, setMousePosX] = useState(0);
+  const [mousePosY, setMousePosY] = useState(0);
+
+  function navClick(event: MouseEvent) {
+    if (window.scrollY <= 90 && router.route == "/") {
+      setMousePosX(event.clientX);
+      setMousePosY(event.clientY);
+
+      setConfetti(true);
+      setTimeout(() => setConfetti(false), 100);
+    }
+  }
+
   return (
     <nav className="navbar bg-base-100 z-50 mx-auto max-w-4xl px-8 my-20">
       <div className="flex-none">
@@ -16,6 +43,7 @@ export default function Navbar() {
       </div>
       <div className="flex-1">
         <Link
+          onClick={navClick}
           href="/"
           className="btn btn-ghost normal-case text-xl bg-gradient-to-r hover:text-transparent from-ctp-red to-ctp-blue bg-clip-text transition-colors duration-75 hover:duration-300"
         >
@@ -34,6 +62,24 @@ export default function Navbar() {
       <div className="flex-none">
         <ThemeSwitch />
       </div>
+      <Confetti
+        style={{ pointerEvents: "none" }}
+        numberOfPieces={confetti ? 200 : 0}
+        initialVelocityX={50}
+        initialVelocityY={-50}
+        ref={confettiRef}
+        gravity={0.05}
+        width={width}
+        height={height}
+        confettiSource={{
+          w: 0,
+          h: 0,
+          x: mousePosX,
+          y: mousePosY,
+        }}
+        recycle={confetti ? true : false}
+        tweenDuration={10}
+      />
     </nav>
   );
 }
