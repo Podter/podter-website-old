e270169bdb7840137396a262839c4c95c998c342
Podter <48936015+Podter@users.noreply.github.com>
2023-02-05 10:03:22 +0700

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,8 +2,12 @@ import Head from "next/head";
 import Container from "@/components/Container";
 import { Icon } from "@iconify/react";
 import questionCircleTwotone from "@iconify/icons-line-md/question-circle-twotone";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <>
       <Head>
@@ -21,6 +25,17 @@ export default function Home() {
         <p className="py-6">
           You are looking for something that doesn&apos;t actually exist.
         </p>
+        <div className="flex flex-row gap-2 w-28">
+          <button
+            className="btn btn-primary w-full"
+            onClick={() => router.back()}
+          >
+            Go back
+          </button>
+          <Link className="btn btn-primary w-full" href="/">
+            Home
+          </Link>
+        </div>
       </Container>
     </>
   );
