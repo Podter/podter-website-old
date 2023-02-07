de888e95ca9b2e352049b9b019660150ad54569d
Podter <48936015+Podter@users.noreply.github.com>
2023-02-07 12:38:20 +0700

    working data fetching

--- pages/guestbook.tsx
+++ pages/guestbook.tsx
@@ -1,14 +1,46 @@
 import Head from "next/head";
 import Container from "@/components/Container";
-import GuestbookMessage from "@/components/GuestbookMessage";
+import GuestbookItem, { GuestbookItemProps } from "@/components/GuestbookItem";
 import githubIcon from "@iconify/icons-fa6-brands/github";
 import discordIcon from "@iconify/icons-fa6-brands/discord";
 import { Icon } from "@iconify/react";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import prisma from "@/lib/prismadb";
 
-export default function Guestbook() {
-  const { data: session, status } = useSession();
+type Data = {
+  id: string;
+  props: GuestbookItemProps;
+};
 
+export const getServerSideProps: GetServerSideProps<{
+  data: Data[];
+}> = async () => {
+  const fetchedData = await prisma.guestbookMessage.findMany();
+  const data: Data[] = [];
+
+  fetchedData.map((item) =>
+    data.push({
+      id: item.id,
+      props: {
+        name: item.name,
+        message: item.message,
+        provider: item.provider === "GITHUB" ? "GitHub" : "Discord",
+        username: item.username,
+        updated: item.updated,
+      },
+    })
+  );
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Guestbook({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <Head>
@@ -23,62 +55,20 @@ export default function Guestbook() {
         </h1>
         <p className="pt-6">Feel free to leave me any message here.</p>
         <div className="flex flex-col md:flex-row gap-2 w-60 pt-3">
-          {status === "unauthenticated" ? (
-            <>
-              <button
-                className="btn w-full gap-3 duration-75 transition-colors"
-                onClick={() => signIn("github")}
-              >
-                Sign in with GitHub
-                <Icon icon={githubIcon} className="h-6 w-6" scale={24} />
-              </button>
-              <button
-                className="btn w-full gap-3 duration-75 transition-colors"
-                onClick={() => signIn("discord")}
-              >
-                Sign in with Discord
-                <Icon icon={discordIcon} className="h-6 w-6" scale={24} />
-              </button>
-            </>
-          ) : status === "authenticated" ? (
-            <button
-              className="btn w-full gap-3 duration-75 transition-colors"
-              onClick={() => signOut()}
-            >
-              Logout
-              <Icon icon={discordIcon} className="h-6 w-6" scale={24} />
-            </button>
-          ) : (
-            ""
-          )}
+          <button className="btn w-full gap-3 duration-75 transition-colors">
+            Sign in with GitHub
+            <Icon icon={githubIcon} className="h-6 w-6" scale={24} />
+          </button>
+          <button className="btn w-full gap-3 duration-75 transition-colors">
+            Sign in with Discord
+            <Icon icon={discordIcon} className="h-6 w-6" scale={24} />
+          </button>
         </div>
         <div className="divider" />
         <div>
-          <GuestbookMessage
-            name="Podter"
-            username="Podter"
-            message="Hello"
-            provider="GitHub"
-            edited={true}
-          />
-          <GuestbookMessage
-            name="Someone"
-            username="Someone#6957"
-            message="Coming soon!"
-            provider="Discord"
-          />
-          <GuestbookMessage
-            name="Lucid"
-            username="lucid_123"
-            message="Hey"
-            provider="GitHub"
-          />
-          <GuestbookMessage
-            name="Podter"
-            username="Podter#5146"
-            message="Test"
-            provider="Discord"
-          />
+          {data.map((item) => (
+            <GuestbookItem key={item.id} {...item.props} />
+          ))}
         </div>
       </Container>
     </>
