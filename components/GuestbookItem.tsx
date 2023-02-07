1f3b2e1e3fd5c1179a16ab2d61b29e4379993fe6
Podter <48936015+Podter@users.noreply.github.com>
2023-02-07 12:37:09 +0700

    changed the name

--- components/GuestbookItem.tsx
+++ components/GuestbookItem.tsx
@@ -1,27 +1,27 @@
 import Shoes from "@/public/shoes.jpg";
 import Image from "next/image";
 
-type GuestbookMessageProps = {
+export type GuestbookItemProps = {
   name: string;
   username: string;
   message: string;
   provider: "GitHub" | "Discord";
-  edited?: boolean;
+  updated?: boolean;
 };
 
-export default function GuestbookMessage({
+export default function GuestbookItem({
   name,
   message,
   provider,
   username,
-  edited = false,
-}: GuestbookMessageProps) {
+  updated = false,
+}: GuestbookItemProps) {
   return (
     <div className="flex flex-col space-y-1 mb-4">
       <div className="flex w-full text-sm items-center gap-2">
         <div
           className="tooltip tooltip-right md:tooltip-top"
-          data-tip={`${username} on ${provider} ${edited ? "(edited)" : ""}`}
+          data-tip={`${username} on ${provider} ${updated ? "(edited)" : ""}`}
         >
           <div className="avatar hover:cursor-pointer">
             <div className="w-6 rounded-full">
