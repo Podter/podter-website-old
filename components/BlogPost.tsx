0ec96b83a76daf5644834459017db1ac8ce76443
Podter <48936015+Podter@users.noreply.github.com>
2023-02-09 15:18:28 +0700

    Update BlogPost.tsx

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -1,4 +1,5 @@
 import Link, { LinkProps } from "next/link";
+import { Eye, CalendarDays } from "lucide-react";
 
 type BlogPostProps = {
   href: LinkProps["href"];
@@ -9,15 +10,25 @@ type BlogPostProps = {
 
 export default function BlogPost({ href, title, date, views }: BlogPostProps) {
   return (
-    <Link className="flex flex-col mb-1" href={href}>
+    <Link className="flex flex-col mb-1 hover:text-ctp-red" href={href}>
       <div className="w-full flex flex-col">
         <h2 className="text-base">{title}</h2>
-        <p className="text-sm text-ctp-subtext0 tracking-tighter">
-          Published on {date}
-        </p>
-        <p className="text-sm text-ctp-subtext0 tracking-tighter">
-          {views} views
-        </p>
+        <div className="flex flex-row gap-2">
+          <p className="text-sm text-ctp-subtext0">
+            <Eye
+              className="inline mr-1 align-[-0.125em] h-[14px] w-[14px]"
+              size={14}
+            />
+            {views} views
+          </p>
+          <p className="text-sm text-ctp-subtext0">
+            <CalendarDays
+              className="inline mr-1 align-[-0.125em] h-[14px] w-[14px]"
+              size={14}
+            />
+            Published on {date}
+          </p>
+        </div>
       </div>
     </Link>
   );
