518053b954ce09b7a9d12730a5c5d416e0d41f57
Podter <48936015+Podter@users.noreply.github.com>
2023-02-10 20:54:12 +0700

    theme switch sound

--- components/Navbar/ThemeSwitch.tsx
+++ components/Navbar/ThemeSwitch.tsx
@@ -1,6 +1,7 @@
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import { useAudio } from "react-use";
 
 export default function ThemeSwitch() {
   const { theme, setTheme } = useTheme();
@@ -10,21 +11,36 @@ export default function ThemeSwitch() {
 
   function changeTheme() {
     if (theme === "light") {
+      offControls.play();
+
       setTheme("dark");
     } else {
+      onControls.play();
+
       setTheme("light");
     }
   }
 
+  const [onAudio, _onState, onControls] = useAudio({
+    src: "/sounds/light-switch-on.mp3",
+  });
+  const [offAudio, _offState, offControls] = useAudio({
+    src: "/sounds/light-switch-off.mp3",
+  });
+
   return (
-    <button
-      className={`btn btn-ghost btn-circle swap swap-rotate transition-all ${
-        currentTheme === "dark" ? "swap-active" : null
-      } transition-none`}
-      onClick={changeTheme}
-    >
-      <Sun className="swap-on h-6 w-6" size={24} />
-      <Moon className="swap-off h-6 w-6" size={24} />
-    </button>
+    <>
+      <button
+        className={`btn btn-ghost btn-circle swap swap-rotate transition-all ${
+          currentTheme === "dark" ? "swap-active" : null
+        } transition-none`}
+        onClick={changeTheme}
+      >
+        <Sun className="swap-on h-6 w-6" size={24} />
+        <Moon className="swap-off h-6 w-6" size={24} />
+      </button>
+      {onAudio}
+      {offAudio}
+    </>
   );
 }
