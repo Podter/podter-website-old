975ac6b2a27dbd8a53e3ed09849fd78ddc420ab9
Podter <48936015+Podter@users.noreply.github.com>
2023-02-12 18:21:00 +0700

    og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,25 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Podter's website" />
+        <meta
+          name="description"
+          content="A student and self taught developer from Thailand."
+        />
+        <meta property="og:title" content="Podter" />
+        <meta
+          property="og:description"
+          content="A student and self taught developer from Thailand."
+        />
+        <meta property="og:url" content="https://podter.xyz/" />
+        <meta property="og:site_name" content="podter.xyz" />
+        <meta
+          property="og:image"
+          content="http://preview.podter.xyz/api/og?title=podter.xyz"
+        />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <body>
