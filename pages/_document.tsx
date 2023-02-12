bf88bb6d1271ca74f8b1d06cfd9f205e6de6f72e
Podter <48936015+Podter@users.noreply.github.com>
2023-02-12 18:39:23 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,15 +14,20 @@ export default function Document() {
           content="A student and self taught developer from Thailand."
         />
         <meta property="og:url" content="https://podter.xyz/" />
-        <meta property="og:site_name" content="podter.xyz" />
+        <meta property="og:site_name" content="Podter" />
+        <meta property="og:locale" content="en-US" />
         <meta
-          property="og:image"
+          property="og:image:url"
           content="http://preview.podter.xyz/api/og?title=podter.xyz"
         />
         <meta property="og:image:width" content="1920" />
         <meta property="og:image:height" content="1080" />
-        <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="googlebot"
+          content="index, follow, max-video-preview:-1, max-image-preview:large, max-snippet:-1"
+        />
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <body>
