d2519becb4d230614a3e62e72c935885e13b7693
Podter <48936015+Podter@users.noreply.github.com>
2023-02-25 10:07:30 +0700

    better transition

--- components/Container.tsx
+++ components/Container.tsx
@@ -8,8 +8,8 @@ type ContainerProps = {
 export default function Container({ children }: ContainerProps) {
   return (
     <div className="h-full lg:px-0 px-10">
-      <div className="flex flex-col max-w-3xl mx-auto mb-16 sm:px-0">
-        <Transition>{children}</Transition>
+      <div className="flex flex-col max-w-3xl mx-auto mb-16 sm:px-0 fade-in">
+        {children}
       </div>
     </div>
   );
--- components/Transition.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode, useEffect, useState } from "react";
-
-type TransitionProps = {
-  children: ReactNode;
-};
-
-export default function Transition({ children }: TransitionProps) {
-  return <div className="fade-in">{children}</div>;
-}
