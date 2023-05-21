0d54e2050ba9841c4949c5d6e99665b8d0289328
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-21 23:04:41 +0700

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,15 @@
+import { TypographyH1, TypographyP } from "@/components/ui/Typography";
+
 export default function Page() {
-  return <div>Home</div>;
+  return (
+    <>
+      <TypographyH1>
+        Hello, I&apos;m Podter.{" "}
+        {/* <Icon className="inline" icon={wavingHand} inline /> */}
+      </TypographyH1>
+      <TypographyP>
+        I&apos;m a student and self-taught developer from Thailand.
+      </TypographyP>
+    </>
+  );
 }
