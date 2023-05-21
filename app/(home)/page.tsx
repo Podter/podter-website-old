0a6755a730a236b31d0326834f5882e4a5952d46
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-21 23:18:05 +0700

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,14 @@
 import { TypographyH1, TypographyP } from "@/components/ui/Typography";
 import { Icon } from "@iconify/react/dist/offline";
 import wavingHand from "@iconify/icons-fluent-emoji-flat/waving-hand";
+import { socials } from "@/constants/socials";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/Tooltip";
+import { Button } from "@/components/ui/Button";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -12,6 +20,26 @@ export default function Page() {
       <TypographyP>
         I&apos;m a student and self-taught developer from Thailand.
       </TypographyP>
+      <div className="flex flex-row gap-3 mt-6">
+        {Object.entries(socials).map(
+          ([social, { url, icon, homePage }], i) =>
+            homePage && (
+              <Tooltip key={i}>
+                <TooltipTrigger>
+                  <Button asChild variant="ghost" className="px-2">
+                    <Link href={url}>
+                      <Icon icon={icon} fontSize={24} className="h-6 w-6" />
+                      <span className="sr-only">{social}</span>
+                    </Link>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{social}</p>
+                </TooltipContent>
+              </Tooltip>
+            )
+        )}
+      </div>
     </>
   );
 }
