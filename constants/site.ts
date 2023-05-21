febac5507f50a4ce6ec57dbd25c7613cd3bfdfa2
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-21 22:43:39 +0700

    seo stuff

---
+++ constants/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: "Podter",
+  username: "Podter",
+  url: "https://podter.me",
+  ogImage: "https://podter.me/og.png",
+  ogApi: "https://podter.me/api/og",
+  description: "A student and self-taught developer from Thailand.",
+};
