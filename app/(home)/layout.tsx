62462f759ac38b6486213ad1cd6942164e4d4ec5
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-21 23:19:36 +0700

    page to layout

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,47 @@
+import { TypographyH1, TypographyP } from "@/components/ui/Typography";
+import { Icon } from "@iconify/react/dist/offline";
+import wavingHand from "@iconify/icons-fluent-emoji-flat/waving-hand";
+import { socials } from "@/constants/socials";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/Tooltip";
+import { Button } from "@/components/ui/Button";
+import Link from "next/link";
+import type { PropsWithChildren } from "react";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <>
+      <TypographyH1>
+        Hello, I&apos;m Podter.{" "}
+        <Icon className="inline" icon={wavingHand} inline />
+      </TypographyH1>
+      <TypographyP>
+        I&apos;m a student and self-taught developer from Thailand.
+      </TypographyP>
+      <div className="flex flex-row gap-3 mt-6">
+        {Object.entries(socials).map(
+          ([social, { url, icon, homePage }], i) =>
+            homePage && (
+              <Tooltip key={i}>
+                <TooltipTrigger>
+                  <Button asChild variant="ghost" className="px-2">
+                    <Link href={url}>
+                      <Icon icon={icon} fontSize={24} className="h-6 w-6" />
+                      <span className="sr-only">{social}</span>
+                    </Link>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{social}</p>
+                </TooltipContent>
+              </Tooltip>
+            )
+        )}
+      </div>
+      {children}
+    </>
+  );
+}
