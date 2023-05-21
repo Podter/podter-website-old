f421816aa4bf2641235b1658398cb441e4bde5ea
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-21 23:17:15 +0700

    fixed slot

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
+import { Slot } from "./Slot";
 import { VariantProps, cva } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
