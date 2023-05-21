5bbfb2e2817f52735cd16ac9e3e987c5eb7d794a
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-21 23:05:11 +0700

    home page emoji

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import { TypographyH1, TypographyP } from "@/components/ui/Typography";
+import { Icon } from "@iconify/react/dist/offline";
+import wavingHand from "@iconify/icons-fluent-emoji-flat/waving-hand";
 
 export default function Page() {
   return (
     <>
       <TypographyH1>
         Hello, I&apos;m Podter.{" "}
-        {/* <Icon className="inline" icon={wavingHand} inline /> */}
+        <Icon className="inline" icon={wavingHand} inline />
       </TypographyH1>
       <TypographyP>
         I&apos;m a student and self-taught developer from Thailand.
