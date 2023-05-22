3d0352456675b5c178f1ad89c1473085f2cc9b66
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-22 20:48:00 +0700

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <>Hello, world!</>;
+import { httpFetch } from "@/lib/utils";
+import { socialId } from "@/constants/socials";
+import type { Data } from "use-lanyard";
+import Lanyard from "./lanyard";
+import { lanyardConfig } from "@/constants/site";
+
+export default async function Page() {
+  const [discord] = await Promise.all([
+    httpFetch<{ data: Data }>(
+      `${lanyardConfig.secure ? "https" : "http"}://${
+        lanyardConfig.hostname
+      }/v1/users/${socialId.discord}`,
+      { cache: "no-store" }
+    ),
+  ]);
+
+  return (
+    <>
+      <div className="flex flex-col md:flex-row gap-4 mt-9">
+        <Lanyard initialData={discord.data} />
+      </div>
+      <div className="flex mt-6"></div>
+    </>
+  );
 }
