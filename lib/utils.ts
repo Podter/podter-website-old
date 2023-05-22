e0f6b086d2050d34386543f858ddf74765affa01
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-22 20:22:15 +0700

    added httpFetch

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function httpFetch<T = any>(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<T> {
+  const res = await fetch(input, init);
+
+  if (!res.ok) {
+    throw new Error("Response is not ok");
+  }
+
+  const json = await res.json();
+
+  return json as T;
+}
