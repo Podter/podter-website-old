326f75934da6bf1e84c19932d67bf3c116d37c14
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-22 21:01:51 +0700

    added roblox data

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,21 +3,25 @@ import { socialId } from "@/constants/socials";
 import type { Data } from "use-lanyard";
 import Lanyard from "./lanyard";
 import { lanyardConfig } from "@/constants/site";
+import getRoblox from "@/lib/getRoblox";
+import Roblox from "./roblox";
 
 export default async function Page() {
-  const [discord] = await Promise.all([
+  const [discord, roblox] = await Promise.all([
     httpFetch<{ data: Data }>(
       `${lanyardConfig.secure ? "https" : "http"}://${
         lanyardConfig.hostname
       }/v1/users/${socialId.discord}`,
       { cache: "no-store" }
     ),
+    getRoblox(),
   ]);
 
   return (
     <>
       <div className="flex flex-col md:flex-row gap-4 mt-9">
         <Lanyard initialData={discord.data} />
+        <Roblox {...roblox} />
       </div>
       <div className="flex mt-6"></div>
     </>
---
+++ app/(home)/roblox.tsx
@@ -0,0 +1,109 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/Card";
+import { TypographyMuted, TypographyP } from "@/components/ui/Typography";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
+import { Separator } from "@/components/ui/Separator";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import robloxPlaceholder from "@/public/roblox-placeholder.png";
+import Link from "next/link";
+
+export default function Roblox(data: RobloxData) {
+  return (
+    <Card className="w-full overflow-hidden relative" asChild>
+      <Link href={`https://www.roblox.com/users/${data.id}/profile`}>
+        <CardHeader className="flex-row items-center justify-between">
+          <div className="flex flex-row items-center gap-2">
+            <div className="flex relative">
+              <Avatar>
+                <AvatarImage
+                  src={data.playerHeadshot}
+                  alt="Roblox profile"
+                  className="bg-muted"
+                />
+                <AvatarFallback>
+                  {data.displayName.charAt(0).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+              <div
+                className={cn(
+                  "rounded-full h-[0.625rem] w-[0.625rem] absolute right-px bottom-px bg-slate-600",
+                  {
+                    "bg-sky-400": data.userPresenceType === "online",
+                    "bg-social-green": data.userPresenceType === "inGame",
+                    "bg-social-yellow": data.userPresenceType === "studio",
+                  }
+                )}
+              />
+            </div>
+            <div className="flex flex-col">
+              <CardTitle asChild>
+                <p>{data.displayName}</p>
+              </CardTitle>
+              <CardDescription>@{data.name}</CardDescription>
+            </div>
+          </div>
+        </CardHeader>
+        {data.userPresenceType === "inGame" ||
+        data.userPresenceType === "studio" ? (
+          <CardContent className="flex items-center gap-4">
+            <Image
+              src={
+                data.placeThumbnail ? data.placeThumbnail : robloxPlaceholder
+              }
+              className="rounded-xl h-16 w-16 bg-muted"
+              alt="Place thumbnail"
+              width={64}
+              height={64}
+            />
+            <Separator orientation="vertical" className="h-20" />
+            <div>
+              <TypographyP className="font-medium">
+                {data.userPresenceType === "studio"
+                  ? "In Roblox Studio"
+                  : "Playing"}
+              </TypographyP>
+              <TypographyMuted>
+                {data.userPresenceType === "studio"
+                  ? data.lastLocation.slice(9)
+                  : data.lastLocation}
+              </TypographyMuted>
+            </div>
+          </CardContent>
+        ) : (
+          <CardContent className="flex items-center gap-4">
+            <Image
+              src={data.playerBody}
+              className="rounded-xl h-16 w-16 bg-muted p-2"
+              alt="Activity image"
+              width={64}
+              height={64}
+            />
+            <Separator orientation="vertical" className="h-20" />
+            <div>
+              <TypographyP className="font-medium">
+                {data.userPresenceType === "online" ? "Online" : "Offline"}
+              </TypographyP>
+              <TypographyMuted>
+                <span className="font-medium">Friends:</span> {data.friendCount}
+              </TypographyMuted>
+              <TypographyMuted>
+                <span className="font-medium">Followers:</span>{" "}
+                {data.followerCount}
+              </TypographyMuted>
+              <TypographyMuted>
+                <span className="font-medium">Last seen:</span>{" "}
+                {data.lastOnline}
+              </TypographyMuted>
+            </div>
+          </CardContent>
+        )}
+      </Link>
+    </Card>
+  );
+}
