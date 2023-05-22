43fe6ec1d784ceadffd0a7714574a34df433e244
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-22 21:01:06 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@ const nextConfig = {
   experimental: {
     mdxRs: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lanyard.podter.xyz",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.rbxcdn.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 const withMDX = require("@next/mdx")();
