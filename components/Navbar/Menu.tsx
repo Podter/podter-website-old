d98461c917dd9681b2da2ed4669fbe7b52069bfb
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-22 18:52:10 +0700

    menu bar

---
+++ components/Navbar/Menu.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import {
+  type Variants,
+  type Transition,
+  AnimatePresence,
+  motion,
+} from "framer-motion";
+import { Button } from "../ui/Button";
+import { ChevronRight, MenuIcon, X } from "lucide-react";
+import { useState } from "react";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/Card";
+import Link from "next/link";
+import { TypographyLarge, TypographyMuted } from "../ui/Typography";
+import { siteConfig } from "@/constants/site";
+import pages from "@/constants/pages";
+
+const menuVariants: Variants = {
+  initial: {
+    scale: 0.75,
+    opacity: 0,
+  },
+  animate: {
+    scale: 1,
+    opacity: 1,
+  },
+  initialBg: {
+    backdropFilter: "blur(0px)",
+  },
+  animateBg: {
+    backdropFilter: "blur(4px)",
+  },
+};
+
+const menuTransition: Transition = {
+  ease: [0.16, 1, 0.3, 1],
+  duration: 0.35,
+};
+
+export default function Menu() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        variant="ghost"
+        className="p-2 sm:hidden mr-2 z-50"
+        onClick={() => setOpen((open) => !open)}
+      >
+        {open ? <X size={24} /> : <MenuIcon size={24} />}
+        <span className="sr-only">Toggle menu</span>
+      </Button>
+      <AnimatePresence>
+        {open && (
+          <>
+            <motion.div
+              className="fixed top-0 left-0 w-full h-full bg-transparent px-8 pb-10 pt-20 origin-top-left sm:hidden z-40"
+              variants={menuVariants}
+              initial="initial"
+              animate="animate"
+              exit="initial"
+              transition={menuTransition}
+            >
+              <Card className="w-full h-full relative">
+                <CardHeader className="pb-4">
+                  <Link
+                    href="/"
+                    className="scroll-m-20 text-2xl font-bold"
+                    onClick={() => setOpen(false)}
+                  >
+                    Podter.
+                  </Link>
+                </CardHeader>
+                <CardContent className="flex flex-col">
+                  {Object.entries(pages).map(([path, { name }], i) => (
+                    <Link
+                      key={i}
+                      href={path}
+                      className="flex flex-row justify-between items-center py-4"
+                      onClick={() => setOpen(false)}
+                    >
+                      <TypographyLarge className="font-medium">
+                        {name}
+                      </TypographyLarge>
+                      <ChevronRight size={24} className="h-6 w-6" />
+                    </Link>
+                  ))}
+                </CardContent>
+                <CardFooter className="absolute bottom-0">
+                  <TypographyMuted>
+                    © 2023 {siteConfig.name} - All right reserved
+                  </TypographyMuted>
+                </CardFooter>
+              </Card>
+            </motion.div>
+            <motion.div
+              className="fixed top-0 left-0 w-full h-full bg-transparent backdrop-blur-sm sm:hidden z-30"
+              variants={menuVariants}
+              initial="initialBg"
+              animate="animateBg"
+              exit="initialBg"
+              transition={menuTransition}
+            />
+          </>
+        )}
+      </AnimatePresence>
+    </>
+  );
+}
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import { LayoutGroup, motion } from "framer-motion";
 import pages from "@/constants/pages";
 import { cn } from "@/lib/utils";
 import ThemeToggle from "./ThemeToggle";
+import Menu from "./Menu";
 
 export default function Navbar() {
   let pathname = usePathname() || "/";
@@ -13,45 +14,48 @@ export default function Navbar() {
 
   return (
     <header className="flex flex-row justify-between items-center w-full h-10 mx-auto max-w-4xl px-8 md:my-20 mt-6 mb-10">
-      <nav className="flex flex-row items-center h-full">
-        <Link
-          href="/"
-          className="mr-3 hover:text-accent-foreground scroll-m-20 text-xl font-bold"
-        >
-          Podter.
-        </Link>
-        <LayoutGroup>
-          {Object.entries(pages).map(([path, { name }], i) => {
-            const isActive = path === pathname;
+      <div className="flex flex-row items-center h-full">
+        <Menu />
+        <nav className="flex flex-row items-center">
+          <Link
+            href="/"
+            className="mr-3 hover:text-accent-foreground scroll-m-20 text-xl font-bold"
+          >
+            Podter.
+          </Link>
+          <LayoutGroup>
+            {Object.entries(pages).map(([path, { name }], i) => {
+              const isActive = path === pathname;
 
-            return (
-              <Link
-                key={i}
-                href={path}
-                className={cn(
-                  "transition-all hover:text-accent-foreground align-middle text-sm font-medium hidden sm:flex",
-                  !isActive && "sm:text-muted-foreground"
-                )}
-              >
-                <span className="relative py-2 px-3">
-                  {name}
-                  {isActive && (
-                    <motion.div
-                      className="absolute inset-0 bg-accent rounded-md -z-10 hidden sm:block"
-                      layoutId="sidebar"
-                      transition={{
-                        type: "spring",
-                        stiffness: 350,
-                        damping: 30,
-                      }}
-                    />
+              return (
+                <Link
+                  key={i}
+                  href={path}
+                  className={cn(
+                    "transition-all hover:text-accent-foreground align-middle text-sm font-medium hidden sm:flex",
+                    !isActive && "sm:text-muted-foreground"
                   )}
-                </span>
-              </Link>
-            );
-          })}
-        </LayoutGroup>
-      </nav>
+                >
+                  <span className="relative py-2 px-3">
+                    {name}
+                    {isActive && (
+                      <motion.div
+                        className="absolute inset-0 bg-accent rounded-md -z-10 hidden sm:block"
+                        layoutId="sidebar"
+                        transition={{
+                          type: "spring",
+                          stiffness: 350,
+                          damping: 30,
+                        }}
+                      />
+                    )}
+                  </span>
+                </Link>
+              );
+            })}
+          </LayoutGroup>
+        </nav>
+      </div>
       <div className="flex flex-row items-center h-full gap-2">
         <ThemeToggle />
       </div>
