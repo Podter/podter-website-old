55cfe441d76c747bb733b5d06d7f70debcbc8c6a
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-26 23:36:19 +0700

    Update socials.tsx

--- app/about/socials.tsx
+++ app/about/socials.tsx
@@ -1,4 +1,4 @@
-import { buttonVariants } from "@/components/ui/Button";
+import { Button } from "@/components/ui/Button";
 import { socials } from "@/constants/socials";
 import { Icon } from "@iconify/react/dist/offline";
 import Link from "next/link";
@@ -9,27 +9,25 @@ export default function Socials() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 w-full items-start mt-6 gap-3">
       {Object.entries(socials).map(([social, { url, icon, username }], i) => (
-        <Link
-          href={url}
-          key={i}
-          className={buttonVariants({
-            variant: "outline",
-            className: "!justify-between h-16",
-          })}
-        >
-          <div className="flex flex-row items-center">
-            <div className="flex flex-row items-center gap-2 cursor-default">
-              <Icon icon={icon} fontSize={24} className="h-6 w-6" />
-              <TypographyMuted className="mr-1 font-medium">
-                {social}:{" "}
-              </TypographyMuted>
+        <Button key={i} variant="outline" className="h-16 shadow-sm">
+          <Link
+            href={url}
+            className="flex flex-row justify-between items-center w-full"
+          >
+            <div className="flex flex-row items-center">
+              <div className="flex flex-row items-center gap-2 cursor-default">
+                <Icon icon={icon} fontSize={24} className="h-6 w-6" />
+                <TypographyMuted className="mr-1 font-medium">
+                  {social}:{" "}
+                </TypographyMuted>
+              </div>
+              <TypographySmall className="font-normal">
+                {username}
+              </TypographySmall>
             </div>
-            <TypographySmall className="font-normal">
-              {username}
-            </TypographySmall>
-          </div>
-          <ExternalLink size={16} className="h-4 w-4" />
-        </Link>
+            <ExternalLink size={16} className="h-4 w-4" />
+          </Link>
+        </Button>
       ))}
     </div>
   );
