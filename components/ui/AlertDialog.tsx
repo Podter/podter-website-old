0f35df74a2e2293b2ef97ae495276aafd068a5ce
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-28 00:18:31 +0700

    Update AlertDialog.tsx

--- components/ui/AlertDialog.tsx
+++ components/ui/AlertDialog.tsx
@@ -113,11 +113,7 @@ const AlertDialogAction = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Action>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>
 >(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Action
-    ref={ref}
-    className={cn(buttonVariants(), className)}
-    {...props}
-  />
+  <AlertDialogPrimitive.Action ref={ref} className={cn(className)} {...props} />
 ));
 AlertDialogAction.displayName = AlertDialogPrimitive.Action.displayName;
 
