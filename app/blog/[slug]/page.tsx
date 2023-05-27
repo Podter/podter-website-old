68f961133e805f2cbe3dbbd77faf68f7b4970dd0
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-27 22:54:11 +0700

    merge scss

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import "./styles.scss";
-import "./highlightjs.scss";
 import { allBlogs } from "contentlayer/generated";
 import { TypographyH1, TypographyMuted } from "@/components/ui/Typography";
 import { CalendarDays } from "lucide-react";
