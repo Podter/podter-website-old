8e418a6fa10ff5ab049bd63e896add73061b2750
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-27 16:44:08 +0700

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,19 @@ import type { PropsWithChildren } from "react";
 import { ThemeProvider } from "next-themes";
 import { TooltipProvider } from "@/components/ui/Tooltip";
 
+if (process.env.NODE_ENV === "production") {
+  console.log(
+    "%c⠀Podter. ",
+    `
+  background: linear-gradient(to right, hsl(347, 87%, 44%), hsl(355, 76%, 59%));
+    color: white;
+    font-weight: bold;
+    font-size: 3rem;
+  `
+  );
+  console.log("A student and self-taught developer from Thailand.");
+}
+
 export default function Providers({ children }: PropsWithChildren) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
