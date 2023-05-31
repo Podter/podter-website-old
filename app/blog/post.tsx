36ea16b275b7cdf7179726af51e9119813c61671
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-05-31 22:43:55 +0700

    better blog post

--- app/blog/post.tsx
+++ app/blog/post.tsx
@@ -4,23 +4,26 @@ import { TypographyP, TypographyMuted } from "@/components/ui/Typography";
 import { CalendarDays } from "lucide-react";
 import { format, parseISO } from "date-fns";
 import ViewCounter from "./views";
+import { Card } from "@/components/ui/Card";
 
 export default function Post({ title, date, url, slug }: Blog) {
   return (
-    <Link href={url} className="flex flex-col mt-6 group w-full gap-1">
-      <TypographyP className="group-hover:opacity-70 transition-opacity">
-        {title}
-      </TypographyP>
-      <div className="flex flex-row w-full gap-2">
-        <ViewCounter slug={slug} />
-        <TypographyMuted>
-          <CalendarDays
-            className="inline mr-1 align-[-0.125em] h-[14px] w-[14px]"
-            size={14}
-          />
-          Published on {format(parseISO(date), "do MMMM, yyyy")}
-        </TypographyMuted>
-      </div>
-    </Link>
+    <Card asChild>
+      <Link href={url} className="flex flex-col mt-6 group w-full gap-1 p-4">
+        <TypographyP className="group-hover:opacity-70 transition-opacity">
+          {title}
+        </TypographyP>
+        <div className="flex sm:flex-row flex-col-reverse w-full sm:gap-2">
+          <ViewCounter slug={slug} />
+          <TypographyMuted>
+            <CalendarDays
+              className="inline mr-1 align-[-0.125em] h-[14px] w-[14px]"
+              size={14}
+            />
+            Published on {format(parseISO(date), "do MMMM, yyyy")}
+          </TypographyMuted>
+        </div>
+      </Link>
+    </Card>
   );
 }
