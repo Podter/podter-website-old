f7ca5388523612406f4d236f2d8d5a67b4c472e8
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-10 13:29:12 +0700

    seo

---
+++ src/constants/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: "Podter",
+  username: "Podter",
+  twitter: "@Real_Podter",
+  url: "https://podter.me",
+  ogImage: "/og.png",
+  ogApi: "/api/og",
+  description: "A student and self-taught developer from Thailand.",
+};
