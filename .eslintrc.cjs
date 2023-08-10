885ce6ff99425e04a6efd932579ce27017e0d3f4
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-10 12:34:28 +0700

    code format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,30 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:react-hooks/recommended",
-        "plugin:astro/recommended",
-        "plugin:prettier/recommended",
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        project: true,
-        tsconfigRootDir: __dirname,
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react-hooks/recommended",
+    "plugin:astro/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["eslint-plugin-jsx-a11y"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
     },
-    plugins: ["eslint-plugin-jsx-a11y"],
-    overrides: [
-        {
-            files: ["*.astro"],
-            parser: "astro-eslint-parser",
-            parserOptions: {
-                parser: "@typescript-eslint/parser",
-                extraFileExtensions: [".astro"],
-            },
-        },
-    ],
+  ],
 };
