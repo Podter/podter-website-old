220b28309cf6cb623cd8f0a68674b76de01cfe0f
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-10 13:00:41 +0700

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
   experimental: {
     assets: true,
   },
-  integrations: [react()],
+  integrations: [react(), tailwind()],
 });
