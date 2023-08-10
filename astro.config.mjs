680a33d26360b40e6c43a1020ab9b7fdc1079967
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-10 13:59:28 +0700

    seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://podter.me",
   output: "hybrid",
   adapter: vercel({
     imageService: true,
@@ -18,5 +21,7 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
+    robotsTxt(),
   ],
 });
