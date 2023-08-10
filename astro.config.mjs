9b7c03d98d2fa099e58254b6c65f8f08abec9056
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-10 12:58:49 +0700

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
+  adapter: vercel({
+    imageService: true,
+  }),
+  experimental: {
+    assets: true,
+  },
   integrations: [react()],
 });
