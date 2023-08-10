901ea9b532bafe1730567d5b049fbcdb2a0e12ad
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-10 22:03:10 +0700

    body layout

--- src/components/counter.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { Button } from "./ui/button";
-
-export default function Counter() {
-  const [count, setCount] = useState(0);
-
-  return <Button onClick={() => setCount((c) => c + 1)}>Count: {count}</Button>;
-}
