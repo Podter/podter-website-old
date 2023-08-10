c5548364cf9812f168372213c0a48c79b56c75bb
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-10 21:39:18 +0700

    use switzer font instead

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ['"Inter Variable"', ...fontFamily.sans],
+        sans: ['"Switzer"', ...fontFamily.sans],
         mono: ['"JetBrains Mono Variable"', ...fontFamily.mono],
       },
       keyframes: {
