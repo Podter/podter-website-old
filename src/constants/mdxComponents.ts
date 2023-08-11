a8fbad7bc6fdb0e8343716e5885f484e8d9b77e3
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-11 22:58:26 +0700

    added mdx

---
+++ src/constants/mdxComponents.ts
@@ -0,0 +1,36 @@
+import {
+  TypographyH1,
+  TypographyH2,
+  TypographyH3,
+  TypographyH4,
+  TypographyP,
+  TypographyBlockquote,
+  TypographyList,
+  TypographyOrderedList,
+  TypographyInlineCode,
+  TypographySmall,
+  // TypographyAnchor,
+} from "@/components/ui/typography";
+import { Table, Tr, Th, Td } from "@/components/ui/table";
+
+const mdxComponents = {
+  // Typography
+  h1: TypographyH1,
+  h2: TypographyH2,
+  h3: TypographyH3,
+  h4: TypographyH4,
+  p: TypographyP,
+  blockquote: TypographyBlockquote,
+  ul: TypographyList,
+  ol: TypographyOrderedList,
+  code: TypographyInlineCode,
+  small: TypographySmall,
+  // a: TypographyAnchor,
+  // Table
+  table: Table,
+  tr: Tr,
+  th: Th,
+  td: Td,
+};
+
+export default mdxComponents;
