7beab18c0018cd9d6641a1176aecfc3da913c51f
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-12 14:48:16 +0700

    better footer

---
+++ src/components/SocialLink.tsx
@@ -0,0 +1,19 @@
+import type { Social } from "@/constants/socials";
+import { Button } from "@/components/ui/button";
+import Iconify from "@/components/ui/iconify";
+
+type Props = {
+  name: string;
+  data: Social;
+};
+
+export default function SocialLink({ name, data }: Props) {
+  return (
+    <Button asChild variant="ghost" size="xs" className="h-8 w-8">
+      <a href={data.url}>
+        <Iconify icon={data.icon} size={16} />
+        <span className="sr-only">{name}</span>
+      </a>
+    </Button>
+  );
+}
