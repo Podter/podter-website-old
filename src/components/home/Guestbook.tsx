a3f4f4e73fc2f726e809c98d788086f20958046f
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-14 21:53:38 +0700

    added error text

--- src/components/home/Guestbook.tsx
+++ src/components/home/Guestbook.tsx
@@ -1,4 +1,5 @@
 import Message, { type MessageData } from "@/components/guestbook/Message";
+import { TypographyP } from "@/components/ui/typography";
 import type { UserData } from "@/lib/getUser";
 import { httpFetch } from "@/lib/utils";
 import useSWR from "swr";
@@ -11,13 +12,19 @@ type GuestbookResponse = {
 };
 
 export default function Guestbook() {
-  const { data, isLoading, error } = useSWR(
+  const { data, isLoading, error } = useSWR<GuestbookResponse, unknown>(
     "/api/guestbook",
     httpFetch<GuestbookResponse>,
   );
 
   if (isLoading || !data) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+  if (error) {
+    return (
+      <TypographyP className="text-destructive mt-3 -mb-3">
+        Oops, something went wrong
+      </TypographyP>
+    );
+  }
 
   return (
     <div className="flex flex-col mt-6 gap-3">
