6ae52795b25d424467c49f9901e902446301a5f7
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-14 15:29:10 +0700

    auth config const

---
+++ src/constants/auth.ts
@@ -0,0 +1,16 @@
+import type { AuthConfig } from "@auth/core";
+import Discord from "@auth/core/providers/discord";
+import GitHub from "@auth/core/providers/github";
+
+export const authConfig: AuthConfig = {
+  providers: [
+    Discord({
+      clientId: import.meta.env.DISCORD_ID,
+      clientSecret: import.meta.env.DISCORD_SECRET,
+    }),
+    GitHub({
+      clientId: import.meta.env.GITHUB_ID,
+      clientSecret: import.meta.env.GITHUB_SECRET,
+    }),
+  ],
+};
--- src/pages/api/auth/[...auth].ts
+++ src/pages/api/auth/[...auth].ts
@@ -1,19 +1,7 @@
 import { AstroAuth } from "auth-astro/server";
-import Discord from "@auth/core/providers/discord";
-import GitHub from "@auth/core/providers/github";
+import { authConfig } from "@/constants/auth";
 
 export const prerender = false;
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
-export const { get, post } = AstroAuth({
-  providers: [
-    Discord({
-      clientId: import.meta.env.DISCORD_ID,
-      clientSecret: import.meta.env.DISCORD_SECRET,
-    }),
-    GitHub({
-      clientId: import.meta.env.GITHUB_ID,
-      clientSecret: import.meta.env.GITHUB_SECRET,
-    }),
-  ],
-});
+export const { get, post } = AstroAuth(authConfig);
