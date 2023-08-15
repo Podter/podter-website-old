f0167b760160821a596c31a24901205e6ac61384
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-15 14:12:06 +0700

    Update getProvider.ts

--- src/lib/getProvider.ts
+++ src/lib/getProvider.ts
@@ -3,25 +3,20 @@ import { httpFetch } from "./utils";
 export default async function getProvider(providerAccountId?: string | null) {
   if (!providerAccountId) return null;
 
-  try {
-    const [discord, github] = await Promise.allSettled([
-      httpFetch(`https://discord.com/api/v9/users/${providerAccountId}`, {
-        headers: {
-          Authorization: `Bot ${import.meta.env.DISCORD_BOT_TOKEN}`,
-        },
-      }),
-      httpFetch(`https://api.github.com/user/${providerAccountId}`),
-    ]);
+  const [discord, github] = await Promise.allSettled([
+    httpFetch(`https://discord.com/api/v9/users/${providerAccountId}`, {
+      headers: {
+        Authorization: `Bot ${import.meta.env.DISCORD_BOT_TOKEN}`,
+      },
+    }),
+    httpFetch(`https://api.github.com/user/${providerAccountId}`),
+  ]);
 
-    if (discord.status === "fulfilled") {
-      return "discord";
-    } else if (github.status === "fulfilled") {
-      return "github";
-    } else {
-      throw new Error("No provider found");
-    }
-  } catch (e) {
-    console.error(e);
+  if (discord.status === "fulfilled") {
+    return "discord";
+  } else if (github.status === "fulfilled") {
+    return "github";
+  } else {
     return null;
   }
 }
