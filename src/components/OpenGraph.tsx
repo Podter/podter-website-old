eeceb7590edbf09595c267cc18ca3a2099c8f3f2
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-19 14:05:27 +0700

    better og

--- src/components/OpenGraph.tsx
+++ src/components/OpenGraph.tsx
@@ -2,34 +2,43 @@
 
 type Props = {
   title: string | null;
+  date: string | null;
 };
 
-export default function OpenGraph({ title }: Props) {
+export default function OpenGraph({ title, date }: Props) {
   return (
     <div
       style={{
+        position: "relative",
         height: "100%",
         width: "100%",
         display: "flex",
-        alignItems: "flex-start",
-        justifyContent: "flex-end",
         // backgroundImage: `url(${siteConfig.url}/og-bg.png)`,
-        backgroundImage: `url("https://podter-website-git-dev-podter.vercel.app/og-bg.png")`,
+        backgroundImage: `url("https://podter-website-4anzff731-podter.vercel.app/og-bg.png")`,
       }}
     >
-      <div
-        style={{
-          marginTop: 64,
-          marginRight: 64,
-          fontSize: 96,
-          fontWeight: 700,
-          color: "#fff",
-          fontFamily: "Switzer",
-          maxWidth: "66.666667%",
-          textAlign: "end",
-        }}
-      >
-        {title}
+      <div tw="absolute flex items-start justify-end top-16 right-16 h-full w-full">
+        <span
+          style={{
+            fontSize: 64,
+            fontFamily: "Switzer",
+            maxWidth: "58.333333%",
+          }}
+          tw="font-bold text-white text-right"
+        >
+          {title}
+        </span>
+      </div>
+      <div tw="absolute flex items-end justify-end bottom-16 right-16 h-full w-full">
+        <span
+          style={{
+            fontSize: 24,
+            fontFamily: "Switzer",
+          }}
+          tw="text-white text-right"
+        >
+          {date}
+        </span>
       </div>
     </div>
   );
--- src/pages/api/og.ts
+++ src/pages/api/og.ts
@@ -1,19 +1,27 @@
 import type { APIRoute } from "astro";
 import satori from "satori";
 import { Resvg } from "@resvg/resvg-js";
-import SwitzerBold from "@/assets/switzer-bold.ttf";
 import OpenGraph from "@/components/OpenGraph";
+import SwitzerRegular from "@/assets/switzer-regular.ttf";
+import SwitzerBold from "@/assets/switzer-bold.ttf";
 
 export const prerender = false;
 
 export const get: APIRoute = async ({ request }) => {
   const { searchParams } = new URL(request.url);
   const title = searchParams.get("title");
+  const date = searchParams.get("date");
 
-  const svg = await satori(OpenGraph({ title }), {
+  const svg = await satori(OpenGraph({ title, date }), {
     width: 1200,
     height: 630,
     fonts: [
+      {
+        name: "Switzer",
+        data: Buffer.from(SwitzerRegular),
+        style: "normal",
+        weight: 400,
+      },
       {
         name: "Switzer",
         data: Buffer.from(SwitzerBold),
