217dd338cfb2ec21fd38716f4cbd6da4da683f59
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-19 14:33:23 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,9 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     sitemap(),
-    robotsTxt(),
+    robotsTxt({
+      policy: [{ allow: ["/", "/api/og"], userAgent: "*" }],
+    }),
     mdx(),
   ],
   markdown: {
