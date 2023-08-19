54ec6b627429571a682bf696e3b311a4f4cd48b6
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-19 14:13:47 +0700

    Update PodterConfetti.tsx

--- src/components/PodterConfetti.tsx
+++ src/components/PodterConfetti.tsx
@@ -1,4 +1,8 @@
-import { useState, useCallback } from "react";
+import {
+  useState,
+  useCallback,
+  type MouseEvent as ReactMouseEvent,
+} from "react";
 import useWindowSize from "@/hooks/useWindowSize";
 import Confetti from "react-confetti";
 
@@ -7,19 +11,20 @@ export default function PodterConfetti() {
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
   const [confetti, setConfetti] = useState(false);
 
-  const triggerConfetti = useCallback(() => {
-    setConfetti(true);
-    setTimeout(() => setConfetti(false), 100);
-  }, [setConfetti]);
+  const triggerConfetti = useCallback(
+    (e: ReactMouseEvent<HTMLButtonElement, MouseEvent>) => {
+      setMouse({ x: e.pageX, y: e.pageY });
+      setConfetti(true);
+      setTimeout(() => setConfetti(false), 100);
+    },
+    [setConfetti],
+  );
 
   return (
     <>
       <button
         className="text-muted-foreground hover:underline underline-offset-4 decoration-foreground/50"
         onClick={triggerConfetti}
-        onMouseMove={(e) => {
-          setMouse({ x: e.pageX, y: e.pageY });
-        }}
       >
         podter._
       </button>
