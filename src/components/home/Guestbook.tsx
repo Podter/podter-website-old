237761e73eb519f472c2cc5746d71431ff1831c1
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-20 14:36:54 +0700

    added guestbook loading spinner

--- src/components/home/Guestbook.tsx
+++ src/components/home/Guestbook.tsx
@@ -1,9 +1,12 @@
+import type { PropsWithChildren } from "react";
 import Message, { type MessageData } from "@/components/guestbook/Message";
 import GuestbookSkeletons from "./GuestbookSkeletons";
 import { TypographyP } from "@/components/ui/typography";
 import type { UserData } from "@/lib/getUser";
 import { httpFetch } from "@/lib/utils";
 import useSWR from "swr";
+import { TypographyHomeSection } from "@/components/ui/typography";
+import Spinner from "@/components/ui/spinner";
 
 type GuestbookResponse = {
   data: {
@@ -12,26 +15,56 @@ type GuestbookResponse = {
   }[];
 };
 
+function GuestbookTitle({ children }: PropsWithChildren) {
+  return (
+    <TypographyHomeSection id="guestbook" className="!mt-10 flex items-center">
+      <a
+        data-rehype-autolink-headings="true"
+        href="#guestbook"
+        className="rehype-autolink-headings mr-2"
+      >
+        Guestbook
+      </a>
+      {children}
+    </TypographyHomeSection>
+  );
+}
+
 export default function Guestbook() {
   const { data, isLoading, error } = useSWR<GuestbookResponse, unknown>(
     "/api/guestbook",
     httpFetch<GuestbookResponse>,
   );
 
-  if (isLoading || !data) return <GuestbookSkeletons />;
+  if (isLoading || !data) {
+    return (
+      <>
+        <GuestbookTitle>
+          <Spinner />
+        </GuestbookTitle>
+        <GuestbookSkeletons />
+      </>
+    );
+  }
   if (error) {
     return (
-      <TypographyP className="text-destructive mt-3 -mb-3">
-        Oops, something went wrong
-      </TypographyP>
+      <>
+        <GuestbookTitle />
+        <TypographyP className="text-destructive mt-3 -mb-3">
+          Oops, something went wrong
+        </TypographyP>
+      </>
     );
   }
 
   return (
-    <div className="flex flex-col mt-6 gap-3">
-      {data.data.map(({ messageData, userData }, i) => (
-        <Message message={messageData} user={userData} key={i} />
-      ))}
-    </div>
+    <>
+      <GuestbookTitle />
+      <div className="flex flex-col mt-6 gap-3">
+        {data.data.map(({ messageData, userData }, i) => (
+          <Message message={messageData} user={userData} key={i} />
+        ))}
+      </div>
+    </>
   );
 }
