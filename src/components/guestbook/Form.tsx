689567fcee1e200f3bfab95288dc9c9b71cb5830
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-20 14:43:44 +0700

    added loading state

--- src/components/guestbook/Form.tsx
+++ src/components/guestbook/Form.tsx
@@ -8,9 +8,10 @@ import { signOut } from "auth-astro/client";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { httpFetch } from "@/lib/utils";
 import Delete from "./Delete";
+import Spinner from "@/components/ui/spinner";
 
 const formSchema = z.object({
   message: z.string(),
@@ -22,6 +23,8 @@ type Props = {
 };
 
 export default function Form({ session, existing }: Props) {
+  const [loading, setLoading] = useState(false);
+
   const { handleSubmit, register } = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -31,20 +34,34 @@ export default function Form({ session, existing }: Props) {
 
   const submit = useCallback(
     async ({ message }: z.infer<typeof formSchema>) => {
-      const formData = new FormData();
-      formData.append("message", message);
-      await httpFetch("/api/guestbook", {
-        method: "POST",
-        body: formData,
-      });
-      location.reload();
+      try {
+        setLoading(true);
+        const formData = new FormData();
+        formData.append("message", message);
+        await httpFetch("/api/guestbook", {
+          method: "POST",
+          body: formData,
+        });
+        location.reload();
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
     },
     [],
   );
 
   const deleteMessage = useCallback(async () => {
-    await httpFetch("/api/guestbook", { method: "DELETE" });
-    location.reload();
+    try {
+      setLoading(true);
+      await httpFetch("/api/guestbook", { method: "DELETE" });
+      location.reload();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
   return (
@@ -59,20 +76,18 @@ export default function Form({ session, existing }: Props) {
         <Input
           className="h-9 px-2 py-1"
           required
+          disabled={loading}
           {...register("message", { required: true })}
         />
-        <Button size="sm" variant="secondary">
-          {existing ? (
-            <>
-              <Edit className="mr-2 h-4 w-4" size={16} />
-              Edit
-            </>
+        <Button size="sm" variant="secondary" disabled={loading}>
+          {loading ? (
+            <Spinner className="mr-2 h-4 w-4" size={16} />
+          ) : existing ? (
+            <Edit className="mr-2 h-4 w-4" size={16} />
           ) : (
-            <>
-              <Edit3 className="mr-2 h-4 w-4" size={16} />
-              Sign
-            </>
+            <Edit3 className="mr-2 h-4 w-4" size={16} />
           )}
+          {existing ? "Edit" : "Sign"}
         </Button>
       </div>
       <div className="flex justify-between">
