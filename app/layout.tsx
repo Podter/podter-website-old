2ce8fed1347ee1831ac0e351221d4b5dd776e686
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-16 13:55:19 +0700

    changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 
 import "./globals.scss";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={GeistSans.className}>{children}</body>
     </html>
   );
 }
