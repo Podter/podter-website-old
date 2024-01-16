2a313c2a185bc74a68996dd37b5d6cbec941f225
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-16 13:35:25 +0700

    initial commit for new site

--- src/pages/api/auth/[...auth].ts
+++
@@ -1,6 +0,0 @@
-import { AstroAuth } from "auth-astro/server";
-
-export const prerender = false;
-
-// eslint-disable-next-line @typescript-eslint/unbound-method
-export const { GET, POST } = AstroAuth();
