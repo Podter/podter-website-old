de9819b5398279fea4ef890c9e9efd59290c38f1
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-16 22:51:03 +0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
 import { createMetadata } from "~/lib/create-metadata";
 import AboutContent from "./about.mdx";
 
+export const dynamic = "force-static";
+
 export const metadata = createMetadata({
   title: "About",
   description: "About page",
