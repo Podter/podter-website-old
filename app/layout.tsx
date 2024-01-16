3a1362d03cd2392352c6a1fa0f9c53cdce62e824
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-16 13:51:59 +0700

    added scss

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
 import { Inter } from "next/font/google";
 
-import "./globals.css";
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
