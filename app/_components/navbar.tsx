ed15bccef292b113b768ad5df1023c3ba99f36a8
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-16 15:37:07 +0700

    Update navbar.tsx

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <header className="mb-10 mt-6 flex h-10 w-full items-center justify-between px-4 md:my-20">
-      Navbar
+      podter._
     </header>
   );
 }
