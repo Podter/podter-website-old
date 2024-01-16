b65335ce2513ea745bfce242386d58f8806a59d3
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-16 16:04:36 +0700

    added button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "~/components/ui/button";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button variant="outline">Hello, world!</Button>
+    </div>
+  );
 }
