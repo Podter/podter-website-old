016e090085faf58ae8240c1fba6b0e50812b58f7
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-17 18:12:30 +0700

    Update typography.tsx

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -102,7 +102,7 @@ export function Code({ className, ...props }: JSX.IntrinsicElements["code"]) {
     <code
       {...props}
       className={cn(
-        "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+        "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm",
         className,
       )}
     />
