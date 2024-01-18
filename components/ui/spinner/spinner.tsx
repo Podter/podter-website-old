c215095f2aab362a3c06ebceac5305e18612738a
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-18 23:40:24 +0700

    spinner update

--- components/ui/spinner/spinner.tsx
+++ components/ui/spinner/spinner.tsx
@@ -4,14 +4,20 @@ import styles from "./spinner.module.scss";
 interface SpinnerProps {
   size?: number;
   className?: string;
+  color?: string;
 }
 
-export default function Spinner({ size = 24, className }: SpinnerProps) {
+export default function Spinner({
+  size = 24,
+  className,
+  color = "hsl(var(--foreground))",
+}: SpinnerProps) {
   return (
     <div
       style={{
         // @ts-expect-error --spinner-size is a custom property
         "--spinner-size": `${size}px`,
+        "--spinner-color": color,
       }}
       className={cn(styles.wrapper, className)}
     >
