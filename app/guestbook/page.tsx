1ddf33a4684a6f21d48e8ad215e653102f6b9665
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-18 22:28:13 +0700

    Update page.tsx

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -1,4 +1,5 @@
-import { H1, P } from "~/components/ui/typography";
+import { Code, H1, P, Pre } from "~/components/ui/typography";
+import { auth } from "~/lib/auth";
 import { createMetadata } from "~/lib/create-metadata";
 
 export const metadata = createMetadata({
@@ -6,13 +7,18 @@ export const metadata = createMetadata({
   description: "Sign my guestbook and leave your mark.",
 });
 
-export default function Guestbook() {
+export default async function Guestbook() {
+  const data = await auth();
+
   return (
     <>
       <div className="flex flex-col">
         <H1>Guestbook</H1>
         <P className="mt-3">Sign my guestbook and leave your mark.</P>
       </div>
+      <Pre>
+        <Code>{JSON.stringify(data)}</Code>
+      </Pre>
     </>
   );
 }
