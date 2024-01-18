8b16f77ab839a0756cd4c9e06ae49506d692e0d2
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-18 22:28:20 +0700

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,35 @@
 import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
 import GitHub from "next-auth/providers/github";
+import { z } from "zod";
 
 import { env } from "~/env.mjs";
+
+const TokenSchema = z.object({
+  email: z.string(),
+  picture: z.string(),
+  sub: z.string(),
+});
+
+declare module "next-auth" {
+  // eslint-disable-next-line no-unused-vars
+  interface Session {
+    user: {
+      user: string;
+      email: string;
+    };
+  }
+}
+
+function getProvider(avatar: string): "github" | "discord" {
+  if (avatar.includes("github")) {
+    return "github";
+  } else if (avatar.includes("discord")) {
+    return "discord";
+  } else {
+    throw new Error("Unknown provider");
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -20,4 +47,22 @@ export const {
     }),
   ],
   session: { strategy: "jwt" },
+  callbacks: {
+    // @ts-expect-error token should be returned
+    session({ session, token: rawToken }) {
+      const token = TokenSchema.parse(rawToken);
+
+      const userId = token.sub;
+      const provider = getProvider(token.picture);
+      const email = token.email;
+
+      return {
+        ...session,
+        user: {
+          user: `${provider}:${userId}`,
+          email,
+        },
+      };
+    },
+  },
 });
