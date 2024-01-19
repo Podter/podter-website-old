0dd89b656a61b5e37cc7c6a81ec6798bf38c5c41
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-19 21:35:14 +0700

    Update skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 import { cn } from "~/lib/utils";
 
 function Skeleton({
@@ -6,7 +8,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      className={cn("animate-pulse rounded-md bg-secondary", className)}
       {...props}
     />
   );
