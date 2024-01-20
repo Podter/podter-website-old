fb488657a319e9a4af61bf9a49506a759e70571d
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-20 15:34:10 +0700

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,19 +1,75 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ExternalLinkIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
+
+import { Button } from "~/components/ui/button";
 import { H1, P } from "~/components/ui/typography";
+import { projects } from "~/constants/projects";
 import { createMetadata } from "~/lib/create-metadata";
 
 export const dynamic = "force-static";
 
 export const metadata = createMetadata({
-  title: "Guestbook",
+  title: "Projects",
   description: "A list of projects I've worked on.",
 });
 
 export default function Projects() {
+  // TODO: make it better (?)
   return (
     <>
       <div className="flex flex-col">
         <H1>Projects</H1>
         <P className="mt-3">A list of projects I&apos;ve worked on.</P>
+      </div>
+      <div className="mt-6 flex flex-col">
+        {projects.map(
+          ({ title, description, img, url, action = "Open", sourceUrl }, i) => (
+            <div key={i} className="mt-10 flex flex-col gap-3 first:mt-0">
+              <div className="aspect-video overflow-hidden rounded-xl md:aspect-[24/9]">
+                <Image
+                  src={img}
+                  alt={title}
+                  className="h-full w-full object-cover transition-transform hover:scale-[1.025]"
+                />
+              </div>
+              <p className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                {title}
+              </p>
+              <p className="leading-7">{description}</p>
+              <div className="flex gap-2">
+                {url && (
+                  <Button asChild size="sm">
+                    <Link href={url}>
+                      <ExternalLinkIcon
+                        className="mr-2 h-4 w-4"
+                        width={16}
+                        height={16}
+                      />
+                      {action}
+                    </Link>
+                  </Button>
+                )}
+                {sourceUrl && (
+                  <Button
+                    asChild
+                    variant={url ? "secondary" : "default"}
+                    size="sm"
+                  >
+                    <Link href={sourceUrl}>
+                      <GitHubLogoIcon
+                        className="mr-2 h-4 w-4"
+                        width={16}
+                        height={16}
+                      />
+                      GitHub
+                    </Link>
+                  </Button>
+                )}
+              </div>
+            </div>
+          ),
+        )}
       </div>
     </>
   );
