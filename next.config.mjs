70fd598638cab2e2385b4ec2fa7233fa5764697b
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-22 01:36:15 +0700

    added bundle-analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
 import mdx from "@next/mdx";
 import million from "million/compiler";
 import withPlugins from "next-compose-plugins";
@@ -18,7 +19,17 @@ const millionConfig = {
   auto: { rsc: true },
 };
 
+/** @type {Parameters<typeof bundleAnalyzer>[0]} */
+const bundleAnalyzerConfig = {
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: true,
+};
+
 export default withPlugins(
-  [() => million.next(nextConfig, millionConfig), mdx()],
+  [
+    () => million.next(nextConfig, millionConfig),
+    mdx(),
+    bundleAnalyzer(bundleAnalyzerConfig),
+  ],
   nextConfig,
 );
