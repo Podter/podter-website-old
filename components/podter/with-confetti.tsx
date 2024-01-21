cd702f4f4f604e3bc2d53243e7463b7d5b3ea339
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-21 23:52:41 +0700

    better confetti

--- components/podter/with-confetti.tsx
+++ components/podter/with-confetti.tsx
@@ -1,6 +1,5 @@
 import type { ElementRef, MouseEventHandler } from "react";
-import { forwardRef, useCallback, useState } from "react";
-import Confetti from "react-confetti";
+import { forwardRef, useCallback } from "react";
 
 import type { BaseLinkProps } from "./base-link";
 import { useWindowSize } from "~/hooks/use-window-size";
@@ -9,37 +8,28 @@ import BaseLink from "./base-link";
 const WithConfetti = forwardRef<ElementRef<typeof BaseLink>, BaseLinkProps>(
   ({ ...props }, ref) => {
     const { width, height } = useWindowSize();
-    const [mouse, setMouse] = useState({ x: 0, y: 0 });
-    const [confetti, setConfetti] = useState(false);
 
-    const triggerConfetti = useCallback<MouseEventHandler<HTMLElement>>((e) => {
-      setMouse({ x: e.pageX, y: e.pageY });
-      setConfetti(true);
-      setTimeout(() => setConfetti(false), 100);
-    }, []);
-
-    return (
-      <>
-        <BaseLink {...props} asButton onClick={triggerConfetti} ref={ref} />
-        <Confetti
-          style={{ position: "fixed" }}
-          numberOfPieces={confetti ? 200 : 0}
-          initialVelocityX={50}
-          initialVelocityY={-50}
-          gravity={0.05}
-          width={width}
-          height={height}
-          confettiSource={{
-            w: 0,
-            h: 0,
-            x: mouse.x,
-            y: mouse.y,
-          }}
-          recycle={confetti}
-          tweenDuration={10}
-        />
-      </>
+    const triggerConfetti = useCallback<MouseEventHandler<HTMLElement>>(
+      async (e) => {
+        const confetti = (await import("canvas-confetti")).default;
+        confetti({
+          particleCount: 75,
+          spread: 100,
+          origin: {
+            x: e.clientX / width,
+            y: e.clientY / height,
+          },
+          startVelocity: 12,
+          gravity: 0.75,
+          decay: 0.95,
+          disableForReducedMotion: true,
+          ticks: 500,
+        });
+      },
+      [width, height],
     );
+
+    return <BaseLink {...props} asButton onClick={triggerConfetti} ref={ref} />;
   },
 );
 WithConfetti.displayName = "WithConfetti";
