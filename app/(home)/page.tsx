17f691bec6518867adca073536ecc97d20e75b8f
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-01-24 23:15:20 +0700

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,21 +9,19 @@ export const runtime = "edge";
 // TODO: add home page
 export default function Home() {
   return (
-    <>
-      <Suspense fallback={<LanyardSkeleton />}>
-        <Lanyard />
-      </Suspense>
-      <div className="flex h-[calc(100vh-12rem)] w-full flex-col items-center justify-center text-center md:h-[calc(100vh-18rem)]">
-        <h1 className="font-heading text-7xl font-semibold md:text-8xl">
-          Podter
-        </h1>
-        <P className="mt-2">
-          A student and self-taught developer from Thailand.
-        </P>
-        <div className="mb-24 mt-6 flex items-center justify-center gap-2 md:mb-48">
+    <div className="flex h-[calc(100vh-10.5rem)] w-full flex-col items-center justify-center text-center md:h-[calc(100vh-16.5rem)]">
+      <h1 className="font-heading text-7xl font-semibold md:text-8xl">
+        Podter
+      </h1>
+      <P className="mt-2">A student and self-taught developer from Thailand.</P>
+      <div className="mb-24 mt-6 flex flex-col gap-4 md:mb-48">
+        <Suspense fallback={<LanyardSkeleton />}>
+          <Lanyard />
+        </Suspense>
+        <div className="flex items-center justify-center gap-2">
           <Socials />
         </div>
       </div>
-    </>
+    </div>
   );
 }
